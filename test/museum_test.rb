3fcb41cce8ea4602f7c420fe6866500a83874dd0
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-07 09:51:27 -0700

    Create tests for Museum class

---
+++ test/museum_test.rb
@@ -0,0 +1,58 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+class MuseumTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_instance_of Museum, dmns
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+    assert_equal [], dmns.exhibits
+  end
+
+  def test_add_exhibit_adds_exhibit
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+  end
+
+  def test_recommended_exhibits_recommends_patron_interest_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 20)
+
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    patron_2 = Patron.new("Sally", 20)
+
+    patron_2.add_interest("IMAX")
+
+    expected_1 = [dead_sea_scrolls, gems_and_minerals]
+
+    assert_equal expected_1, dmns.recommend_exhibits(patron_1)
+
+    expected_2 = [imax]
+
+    assert_equal expected_2, dmns.recommend_exhibits(patron_2)
+  end
+end
