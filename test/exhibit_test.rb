dc1379a62a939b6c490b326eafcc198714ad6213
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-07 10:11:24 -0700

    Create tests for Museum class and fix minor issue in Exhibit class

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -6,7 +6,7 @@ class ExhibitTest < Minitest::Test
   def test_it_exists_and_has_attributes
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 
-    assert_it_exists
+    assert_instance_of Exhibit, exhibit
     assert_equal "Gems and Minerals", exhibit.name
     assert_equal 0, exhibit.cost
   end
